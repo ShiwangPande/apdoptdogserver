d30c750e00eeaeb8f6ae994f2d36117871ac351a
shiwang pandey <110737840+ShiwangPande@users.noreply.github.com>
2024-05-23 02:58:31 +0530

    first comment

--- server.js
+++ server.js
@@ -122,5 +122,5 @@ app.delete('/api/pets/:id', (req, res) => {
 });
 
 // Start the Express server
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
